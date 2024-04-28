24ae5100e0f65e401748c358c123f2ada0c045b1
mohtar25 <mokhtar2000naj@gmail.com>
2024-04-28 21:28:35 +0200

    correct format

--- knight.js
+++ knight.js
@@ -1,45 +1,49 @@
 // empty 0 // visited 1 // knight 2
 
-export default function PossibleMoves(x, y) {
+export default function PossibleMoves(to) {
+  const x = to[0];
+  const y = to[1];
   const possiblemoves = [];
   // upleft
   if (y + 2 < 8 && x + 1 < 8) {
-    possiblemoves.push([[x + 1], [y + 2]]);
+    possiblemoves.push([x + 1, y + 2]);
   }
 
   // upright
   if (y + 2 < 8 && x - 1 >= 0) {
-    possiblemoves.push([[x - 1], [y + 2]]);
+    possiblemoves.push([x - 1, y + 2]);
   }
 
   // downleft
   if (x + 1 < 8 && y - 2 >= 0) {
-    possiblemoves.push([[x + 1], [y - 2]]);
+    possiblemoves.push([x + 1, y - 2]);
   }
 
   // downright
   if (x - 1 >= 0 && y - 2 >= 0) {
-    possiblemoves.push([[x - 1], [y - 2]]);
+    possiblemoves.push([x - 1, y - 2]);
   }
 
   // rightup
   if (x + 2 < 8 && y + 1 < 8) {
-    possiblemoves.push([[x + 2], [y + 1]]);
+    possiblemoves.push([x + 2, y + 1]);
   }
 
   // rightdown
   if (x + 2 < 8 && y - 1 >= 0) {
-    possiblemoves.push([[x + 2], [y - 1]]);
+    possiblemoves.push([x + 2, y - 1]);
   }
 
   // leftup
   if (x - 2 >= 0 && y + 1 < 8) {
-    possiblemoves.push([[x - 2], [y + 1]]);
+    possiblemoves.push([x - 2, y + 1]);
   }
 
   // leftdown
   if (x - 2 >= 0 && y - 1 >= 0) {
-    possiblemoves.push([[x - 2], [y - 1]]);
+    possiblemoves.push([x - 2, y - 1]);
   }
   return possiblemoves;
 }
+
+console.log(PossibleMoves([3, 1]));
--- main.js
+++ main.js
@@ -1,3 +1,20 @@
 import PossibleMoves from "./knight.js";
 
-console.log(PossibleMoves(2, 3));
+function Findshortest(from, to) {
+  if (from === to) return true;
+  console.log(from);
+  const possibleMoves = PossibleMoves(from);
+
+  console.log("=====po======\n", possibleMoves);
+  // while (possibleMoves.length !== 0) {
+
+  if (possibleMoves) {
+    const cur = possibleMoves.shift();
+    console.log(cur, "==================");
+    return Findshortest(cur[0], to);
+  }
+
+  // }
+}
+
+console.log(Findshortest([1, 1], [3, 5]));
