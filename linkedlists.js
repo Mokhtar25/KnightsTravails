0b1154cbc777e33b34b2c820f2e6824b2fff48a6
mohtar25 <mokhtar2000naj@gmail.com>
2024-04-29 17:56:17 +0200

    adding linked lists and specfic functions for the usecase of the knight

---
+++ linkedlists.js
@@ -0,0 +1,62 @@
+function Node(data) {
+  let value = data;
+  let next = null;
+
+  return { value, next };
+}
+
+function LinkedList() {
+  let head = null;
+  let tail = null;
+  let index = -1;
+
+  const push = (data) => {
+    const newNode = Node(data);
+    if (head === null) {
+      head = newNode;
+    } else {
+      tail.next = newNode;
+    }
+    tail = newNode;
+    index++;
+  };
+  const findSquare = (value) => {
+    let cur = head;
+    try {
+      while (cur) {
+        if (cur.value[0] === value[0] && cur.value[1] === value[1]) {
+          return true;
+        }
+        cur = cur.next;
+      }
+    } catch (e) {
+      return e;
+    }
+  };
+  const shift = () => {
+    const tmp = head;
+    head = tmp.next;
+    return tmp;
+  };
+  const print = () => {
+    let cur = head;
+    while (cur) {
+      console.log(cur.value);
+      cur = cur.next;
+    }
+  };
+  const del = () => {
+    console.log(head, "\n", tail, index);
+  };
+
+  return { head, tail, push, shift, del, print, findSquare };
+}
+
+// const ara = LinkedList();
+// ara.push([1, 2]);
+// ara.push([1, 3]);
+//
+// console.log(ara.findSquare([1, 3]));
+// const item = ara.shift();
+// console.log("this is item: ", item);
+// ara.print();
--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 import PossibleMoves from "./knight.js";
+// create a tree and add every possible node to it in matter of six times and then search the tree for the
+// given square and the square wanted. 6 maximum times for the knight to move to any square. or try a linked list
+// and save the nodes in a hash map and find the shortest possible moves. make puckets and put all the possible moves and
+// try to get every possible move in a hasmap and a linkde list and try to save the stuff into the hash map
 
 function Findshortest(from, to) {
   if (from[0] === to[0] && from[1] === to[1]) return true;
@@ -12,4 +16,4 @@ function Findshortest(from, to) {
   }
 }
 
-console.log(Findshortest([1, 1], [3, 5]));
+console.log(Findshortest([0, 0], [3, 3]));
